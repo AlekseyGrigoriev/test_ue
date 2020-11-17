37d6578443a8d5ed6caa132ddb7e59c622dfff4e
Alexey Grigoriev <a_grigoriev@wargaming.net>
2020-11-17 17:08:00 +0800

    origin_commit_1

---
+++ source.cpp
@@ -0,0 +1,79 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "BaseHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+#include "Screens/BattleScreen.h"
+
+
+void ABaseHUD::Init(APawn* OwningPawn)
+{
+	CreateScreenWidgets();
+	UBattleScreen* BattleScreen = GetScreen<UBattleScreen>();
+	if (IsValid(BattleScreen))
+	{
+	    BattleScreen->InitByPawn(OwningPawn);
+	}
+}
+
+void ABaseHUD::ShowHUD()
+{
+    Super::ShowHUD();
+
+    SetBattleScreenVisibility(bShowHUD);
+}
+
+void ABaseHUD::HideHUD()
+{
+    SetBattleScreenVisibility(false);
+}
+
+void ABaseHUD::ShowBattleScreen()
+{
+    SetBattleScreenVisibility(true);
+}
+
+void ABaseHUD::SetBattleScreenVisibility(const bool bShouldBeVisible)
+{
+    UBattleScreen* BattleScreen = GetScreen<UBattleScreen>();
+    if (IsValid(BattleScreen))
+    {
+        BattleScreen->SetVisibility(bShouldBeVisible ? ESlateVisibility::SelfHitTestInvisible : ESlateVisibility::Collapsed);
+    }
+    
+    GetOwningPlayerController()->SetVirtualJoystickVisibility(bShouldBeVisible);
+}
+
+void ABaseHUD::AddWidgetToScreen(TSubclassOf<UUserWidget> WidgetClass)
+{
+	if (!ScreensMap.Contains(WidgetClass))
+	{
+		UUserWidget* ScreenWidget = CreateWidget(GetOwningPlayerController(), WidgetClass);
+		if (IsValid(ScreenWidget))
+		{
+			ScreenWidget->AddToViewport();
+			ScreensMap.Add(WidgetClass, ScreenWidget);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Couldn't create widget for class %s"), *WidgetClass->GetName());
+		}
+	}
+}
+
+void ABaseHUD::CreateScreenWidgets()
+{
+	for (TSubclassOf<UUserWidget> WidgetClass : InitialScreensClasses)
+	{
+		if (!IsValid(WidgetClass))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Invalid WidgetClass in InitialScreensClasses"));
+			continue;
+		}
+		AddWidgetToScreen(WidgetClass);
+	}
+	
+#if TS_BUILD_CHEATS
+	AddWidgetToScreen(CheatPanelClass);
+#endif
+}
